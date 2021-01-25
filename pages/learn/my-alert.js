df721fd1c739aa5630591490059aae2943062cc5
lhchen74 <2938449682@qq.com>
2021-01-25 21:53:45 +0800

    nextjs start

---
+++ pages/learn/my-alert.js
@@ -0,0 +1,10 @@
+import Alert from './alert'
+
+export default function MyAlert() {
+    return (
+        <div>
+            <Alert type="success">Success</Alert>
+            <Alert type="error">Error</Alert>
+        </div>
+    )
+}
